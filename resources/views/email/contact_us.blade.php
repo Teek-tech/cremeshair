ea69c6d21c42d7b39a4efeb9c6b1b801a8881f22
Tkay <tochukwuodoeme@gmail.com>
2020-01-07 10:48:46 +0100

    fix mail

--- resources/views/email/contact_us.blade.php
+++ resources/views/email/contact_us.blade.php
@@ -1,11 +1,11 @@
-<b>Hello {{auth()->user()->name}},</b> <br>
+<b>Hello {{auth()->user()->name}},</b><br><br>
 <b>New Message:</b> <br>
 <b>From: {{$contactUs['name']}}</b> <br>
-<b>Email: {{$contactUs['email']}}</b><br>
+<b>Email: {{$contactUs['email']}}</b><br><br>
 
-<b><u>{{$contactUs['subject']}}</u></b><br>
-<b><i>{{$contactUs['message']}}</i></b>
+<b><u>{{$contactUs['subject']}}</u></b>
+<i>{{$contactUs['message']}}</i>
 <br><br>
 
-<a href="#">Cremes.</a>
+<a href="https://cremeshair.com">Cremes.</a>
 
